b225d06b8f556e8383ec231bea100065babc0192
Juan Barba <barbajuan11@gmail.com>
2022-11-14 22:05:18 -0800

    Added Workout and Goal models to backend

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Workout (models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+
+class Goal (models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    completion_status = models.BooleanField(default=False)
+    category = models.CharField(max_length=10)
+    start_status = models.FloatField()
+    current_status = models.FloatField()
+    target_status = models.FloatField()
+
+
