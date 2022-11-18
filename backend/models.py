d3477c2ffae4c8ff98556a411322fa10033e3081
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2022-11-17 21:54:24 -0800

    added StatSheet and updated other models

--- backend/models.py
+++ backend/models.py
@@ -16,10 +16,12 @@ class ActivityLog(models.Model):
     weight = models.FloatField(blank=True)
     reps = models.IntegerField(blank=True)
     distance = models.FloatField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activity_log')
 
 class Workout (models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(blank=True)
+    users = models.ManyToManyField(User, related_name='workouts')
 
 class Goal (models.Model):
     name = models.CharField(max_length=30)
@@ -29,5 +31,12 @@ class Goal (models.Model):
     start_status = models.FloatField()
     current_status = models.FloatField()
     target_status = models.FloatField()
+
+class StatSheet (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='stat_sheet')
+    weight = models.FloatField(blank=True)
+    height = models.FloatField(blank=True)
+    dream_bod = models.URLField(max_length=200, blank=True)
+    # do bmi in frontend
 
 
