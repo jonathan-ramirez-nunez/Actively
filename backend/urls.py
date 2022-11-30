52280146990c04353f6b1bd58809ddf41375ca1e
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2022-11-29 22:04:22 -0800

    Created a UsersView.

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, UsersView
 
 urlpatterns = [
     path('user', UserView.as_view()),
+    path('users', UsersView.as_view()),
+
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework import status
+from rest_framework import status, generics
 from .serializers import UserSerializer, ActivityLogSerializer, WorkoutSerializer, GoalSerializer, StatSheetSerializer
 from .models import User, ActivityLog, Workout, Goal, StatSheet
 from rest_framework.views import APIView
@@ -22,4 +22,8 @@ class UserView(APIView):
                 return Response(data, status=status.HTTP_200_OK)
             return Response({'User Not Found': 'Invalid User ID.'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request': 'User ID not found in request.'}, status=status.HTTP_400_BAD_REQUEST)
+
+class UsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
