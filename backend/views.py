56cedc581fa5ff0fac84ab127af2ac2aeff3b0d2
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2022-12-01 22:11:10 -0800

    Updated UserView

--- backend/views.py
+++ backend/views.py
@@ -7,6 +7,7 @@ from .models import User, ActivityLog, Workout, Goal, StatSheet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 class UserView(APIView):
@@ -22,6 +23,18 @@ class UserView(APIView):
                 return Response(data, status=status.HTTP_200_OK)
             return Response({'User Not Found': 'Invalid User ID.'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request': 'User ID not found in request.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, format=None):
+        """ if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create() """
+        
+        serializer = self.serialzier_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response({"Bad Request": "User could not be created with given entries."}, status=status.HTTP_400_BAD_REQUEST)
+        
 
 class UsersView(generics.ListAPIView):
     queryset = User.objects.all()
