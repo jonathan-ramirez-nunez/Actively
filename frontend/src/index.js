a7a92685722162ff63aa12eacd5d3ba35403ae5e
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2022-12-14 20:48:12 -0800

    created frontend with a test component to render

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+
+function index() {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+//ReactDOM.render(<App />, document.getElementById('root'));
