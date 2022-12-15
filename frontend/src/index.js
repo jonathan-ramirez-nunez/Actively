74a61040e0b81ec23bd7cc1cbace08687e1e63f0
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2022-12-14 22:34:28 -0800

    added functionality for SASS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import "../static/style.scss";
 
-function index() {
-    ReactDOM.render(<App />, document.getElementById('root'));
-}
-//ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -23,7 +23,18 @@ module.exports = {
                         presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              }
         ]
     },
     resolve: {
