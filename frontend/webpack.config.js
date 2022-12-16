257c641a06648dd5640aeb176914ea9771a58c19
Juan Barba <barbajuan11@gmail.com>
2022-12-15 21:06:01 -0800

    Modified files to work as jsx and added home page component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
